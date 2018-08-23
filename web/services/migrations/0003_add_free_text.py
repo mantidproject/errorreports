28a2d77b9ac25fffda86a4e232c7405709882216
Keith Butler <keith.butler@stfc.ac.uk>
2018-08-23 14:24:22 +0100

    Changed migrations file to add text box

--- web/services/migrations/0003_add_free_text.py
+++ web/services/migrations/0003_add_free_text.py
@@ -12,10 +12,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='errorreport',
-            name='textbox',
-        ),
         migrations.AddField(
             model_name='errorreport',
             name='textBox',
