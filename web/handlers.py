ea04936ce11cc8caafe8f3e534c00507ca295efa
Mantid-Matthew <matthew.andrew@stfc.ac.uk>
2020-03-10 12:59:24 +0000

    Added new files for slack notifications

---
+++ web/handlers.py
@@ -0,0 +1,20 @@
+import logging
+from services.tasks import send_logging_output_to_slack
+import threading
+
+logger = logging.getLogger(__name__)
+
+
+class SlackHandler(logging.Handler):
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            notification_thread = threading.Thread(
+                target=send_logging_output_to_slack, args=(msg,))
+            notification_thread.start()
+        except Exception as e:
+            # This logger is not Django logger so will
+            # not come back here causing recurion.
+            # Will instead be handled by the Root
+            # logger which prints to console
+            logger.error({e})
