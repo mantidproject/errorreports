a05f83dd9633bf660ccbd8a0d8fdbda6f77b91c1
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2025-01-10 11:19:11 +0000

    add check to avoid out of bounds error

--- web/services/utils/handel_compressed_cpp_traces.py
+++ web/services/utils/handel_compressed_cpp_traces.py
@@ -11,9 +11,12 @@ def extract_mantid_code_threads_from_cpp_traces(compressed_cpp_traces: str):
     """
     cpp_traces_from_pystack = zlib.decompress(
         base64.standard_b64decode(compressed_cpp_traces)).decode("utf-8")
+    traces = re.split(r'\nTraceback for ', cpp_traces_from_pystack)
+    if len(traces > 1):
+        # Trim boilerplate output
+        traces = traces[1:]
     return ["Traceback for " + trace_back for trace_back in
-            re.split(r'\nTraceback for ', cpp_traces_from_pystack)[1:] if
-            _search_for_mantid_codein_trace(trace_back)]
+            traces if _search_for_mantid_codein_trace(trace_back)]
 
 
 def _search_for_mantid_codein_trace(trace_back: str) -> bool:
