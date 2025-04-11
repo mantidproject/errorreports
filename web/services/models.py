0e71ea9e3d8df9c0b30fea914dc7708119cd08e5
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2025-04-11 11:21:49 +0100

    move model constants to file

--- web/services/models.py
+++ web/services/models.py
@@ -3,7 +3,27 @@ from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.db.models import signals
 from services.tasks import send_notification_to_slack
-from services.constants import input_box_max_length, free_text_max_length
+from services.constants import (
+    uid_max_length,
+    host_max_length,
+    osName_max_length,
+    osArch_max_length,
+    osVersion_max_length,
+    paraView_max_length,
+    mantidVersion_max_length,
+    mantidSha1_max_length,
+    osReadable_max_length,
+    application_max_length,
+    facility_max_length,
+    exitCode_max_length,
+    upTime_max_length,
+    free_text_max_length,
+    stacktrace_max_length,
+    cppCrompressedTraces_max_length,
+    input_box_max_length,
+    repoName_max_length,
+    issueNumber_max_length
+)
 from services.utils.handel_compressed_cpp_traces import (
     extract_mantid_code_threads_from_cpp_traces
 )
@@ -19,30 +39,36 @@ TEST_VALUES = ('public_email', 'Not provided', '', 'Something went wrong')
 
 class ErrorReport(models.Model):
     # md5 ex: "c5a9b601408709f47417bcba3571262b"
-    uid = models.CharField(max_length=32, help_text="md5 version of username")
+    uid = models.CharField(max_length=uid_max_length,
+                           help_text="md5 version of username")
     # md5 ex: "7defb184ceadab4e79eff323359ad373"
-    host = models.CharField(max_length=32, help_text="md5 version of hostname")
+    host = models.CharField(max_length=host_max_length,
+                            help_text="md5 version of hostname")
     # ex: "2014-12-08T18:50:35.817942000"
     dateTime = models.DateTimeField(db_index=True)
-    osName = models.CharField(max_length=32)  # ex: "Linux"
-    osArch = models.CharField(max_length=16)  # ex: "x86_64"
+    osName = models.CharField(max_length=osName_max_length)  # ex: "Linux"
+    osArch = models.CharField(max_length=osArch_max_length)  # ex: "x86_64"
     # ex: "3.17.4-200.fc20.x86_64"
-    osVersion = models.CharField(max_length=32)
-    ParaView = models.CharField(max_length=16)  # ex: "3.98.1"
-    mantidVersion = models.CharField(max_length=64)  # ex: "3.2.20141208.1820"
+    osVersion = models.CharField(max_length=osVersion_max_length)
+    ParaView = models.CharField(max_length=paraView_max_length)  # ex: "3.98.1"
+    # ex: "3.2.20141208.1820"
+    mantidVersion = models.CharField(max_length=mantidVersion_max_length)
     # sha1 ex: "e9423bdb34b07213a69caa90913e40307c17c6cc"
-    mantidSha1 = models.CharField(max_length=40,
+    mantidSha1 = models.CharField(max_length=mantidSha1_max_length,
                                   help_text="sha1 for specific mantid version")
     # ex: "Fedora 20 (Heisenbug)"
-    osReadable = models.CharField(max_length=80, default="", blank=True)
-    application = models.CharField(max_length=80, default="", blank=True)
+    osReadable = models.CharField(max_length=osReadable_max_length, default="",
+                                  blank=True)
+    application = models.CharField(max_length=application_max_length,
+                                   default="", blank=True)
 
-    facility = models.CharField(max_length=32, default="", blank=True)
-    exitCode = models.CharField(max_length=32,
+    facility = models.CharField(max_length=facility_max_length, default="",
+                                blank=True)
+    exitCode = models.CharField(max_length=exitCode_max_length,
                                 default="",
                                 null=True,
                                 blank=True)
-    upTime = models.CharField(max_length=32, default="")
+    upTime = models.CharField(max_length=upTime_max_length, default="")
     user = models.ForeignKey('UserDetails',
                              on_delete=models.SET_NULL,
                              blank=True,
@@ -50,9 +76,11 @@ class ErrorReport(models.Model):
     textBox = models.CharField(max_length=free_text_max_length,
                                default="",
                                null="True")
-    stacktrace = models.CharField(max_length=10000, default="")
-    cppCompressedTraces = models.CharField(max_length=10000, default="",
-                                           blank=True)
+    stacktrace = models.CharField(max_length=stacktrace_max_length, default="")
+    cppCompressedTraces = models.CharField(
+        max_length=cppCrompressedTraces_max_length,
+        default="",
+        blank=True)
     githubIssue = models.ForeignKey('GithubIssue',
                                     on_delete=models.SET_NULL,
                                     blank=True,
@@ -85,12 +113,13 @@ class UserDetails(models.Model):
 
 
 class GithubIssue(models.Model):
-    repoName = models.CharField(max_length=200,
+    repoName = models.CharField(max_length=repoName_max_length,
                                 default="",
                                 blank=True,
                                 help_text="'user/repo_name': for example "
                                           "'mantidproject/mantid'")
-    issueNumber = models.CharField(max_length=16, default="", blank=True)
+    issueNumber = models.CharField(max_length=issueNumber_max_length,
+                                   default="", blank=True)
 
 
 def notify_report_received(sender, instance, signal, *args, **kwargs):
