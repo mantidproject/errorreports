5c435b200672c63537d70e66db0090193942f081
Waruna Wickramasingha <waruna.wickramasingha@stfc.ac.uk>
2025-09-05 10:46:27 +0100

    rebased to main

--- web/urls.py
+++ web/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 from django.urls import path
+from views import DRFLoginView
 
 admin.autodiscover()
 
@@ -10,6 +11,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("services.urls")),
     # should be in services/urls.py
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    path("", RedirectView.as_view(url="/api/", permanent=True)),
+    path('api-auth/login/', DRFLoginView.as_view(), name="rest_login"),
+    path('api-auth/', include('rest_framework.urls',
+                              namespace='rest_framework')),
+    path('', RedirectView.as_view(url='/api/', permanent=True))
 ]
