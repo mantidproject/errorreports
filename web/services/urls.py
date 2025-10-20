cdf01f5eb73da49a82f2661a96538cdc6266f6b4
warunawickramasingha <waruna.wickramasingha@stfc.ac.uk>
2025-10-20 16:38:02 +0100

    add basename to the viewset

--- web/services/urls.py
+++ web/services/urls.py
@@ -3,7 +3,7 @@ from services import views
 from django.urls import path
 
 router = routers.DefaultRouter(trailing_slash=False)
-router.register(r"error", views.ErrorViewSet)
+router.register(r"error", views.ErrorViewSet, basename="error")
 urlpatterns = [
     path("", views.api_root),
     path("recovery/", views.RecoveryFileUploadView.as_view()),
